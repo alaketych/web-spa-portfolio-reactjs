b8633a94cc1fe02440c814513189c1f20ebffa48
Alexandr Laketych <alaketych@ukr.net>
2019-08-11 12:40:24 +0300

    Update NewSubProject.js

--- src/components/NewSubProject.js
+++ src/components/NewSubProject.js
@@ -1,25 +1,5 @@
 import React, { Component } from "react";
 
-// class NewSubProject extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <div className="gallery-image">
-//                     <div className="img-box">
-//                         <img src={require("../images/1.jpg")} />
-//                         <div className="transparent-box">
-//                             <div className="caption">
-//                                 <p>Library</p>
-//                                 <p className="opacity-low">Architect Design</p>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-
 function NewSubProject(props) {
     return (
         <div className="gallery-image">
